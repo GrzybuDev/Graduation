f2d569559e7f2a9ee8e25eef1f7f49c819d7e5ab
GrzybuDev <76538812+GrzybuDev@users.noreply.github.com>
2021-03-29 22:39:03 +0200

    Poprawa błędów w usuń

--- app/src/main/java/com/example/lendall/room/item/ItemDatabase.kt
+++ app/src/main/java/com/example/lendall/room/item/ItemDatabase.kt
@@ -1,4 +1,5 @@
 package com.example.lendall.room.item
+
 import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
@@ -7,6 +8,7 @@ import androidx.room.RoomDatabase
 @Database(entities = [Item::class], version = 2, exportSchema = false)
 abstract class ItemDatabase: RoomDatabase() {
    abstract fun ItemDao(): ItemDao
+
    companion object{
        private var instance: ItemDatabase? = null
        fun getInstance(context: Context): ItemDatabase?{
